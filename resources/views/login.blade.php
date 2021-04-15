9e65b72219f6261bc15c036881f9f3632a38a69b
PacP101 <perezcamilo654@gmail.com>
2021-04-14 20:01:11 -0500

    full

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,44 +10,48 @@
     <div class="container">
         <div class="row">
 			<div class="col-md-5 py-4 mx-auto">
-			<div id="first">
-				<div class="myform form ">
-                    <div class = "card card-primary">
-                        <div class="col-md-12">
-					 <div class="logo mb-5">
-						 <div class="col-md-12 text-center">
-                            <div class="card-header" >
-							    <h1>Basic Ingles</h1>
+			    <div id="first">
+				    <div class="myform form ">
+                        <div class = "card card-primary">
+                            <div class="col-md-12">
+					            <div class="logo mb-5">
+						            <div class="col-md-12 text-center">
+                                        <div class="card-header" >
+							                <h1>Basic Ingles</h1>
+                                        </div>
+						            </div>
+                                        <form action="" method="post" name="login">
+                                            <div class="form-group">
+                                                <label for="exampleInputEmail1">Dirección de correo electrónico</label>
+                                                <input type="email" name="email"  class="form-control" id="email" aria-describedby="emailHelp" placeholder="Ingrese correo electrónico">
+                                            </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputEmail1">Contraseña</label>
+                                                    <input type="password" name="password" id="password"  class="form-control" aria-describedby="emailHelp" placeholder="Ingrese Contraseña">
+                                                </div>
+                                                <div class="form-group">
+                                                    <p class="text-center">Al registrarte aceptas nuestra <a href="#">Condiciones de uso</a></p>
+                                                </div>
+                                                    <div class="col-md-12 text-center ">
+                                                        <button type="submit" class=" btn btn-block mybtn btn-primary tx-tfm">Acceder</button>
+                                                    </div>
+                                                    <div class="col-md-12 ">
+                                                        <div class="login-or">
+                                                            <hr class="hr-or">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <p class="text-center">¿No tienes cuenta? <a href="#" id="signup">Registrate aquí</a></p>
+
+                                                    </div>
+                                        </form>
+                                </div>
                             </div>
-						 </div>
-                        <form action="" method="post" name="login">
-                           <div class="form-group">
-                              <label for="exampleInputEmail1">Dirección de correo electrónico</label>
-                              <input type="email" name="email"  class="form-control" id="email" aria-describedby="emailHelp" placeholder="Ingrese correo electrónico">
-                           </div>
-                           <div class="form-group">
-                              <label for="exampleInputEmail1">Contraseña</label>
-                              <input type="password" name="password" id="password"  class="form-control" aria-describedby="emailHelp" placeholder="Ingrese Contraseña">
-                           </div>
-                           <div class="form-group">
-                              <p class="text-center">Al registrarte aceptas nuestra <a href="#">Condiciones de uso</a></p>
-                           </div>
-                           <div class="col-md-12 text-center ">
-                              <button type="submit" class=" btn btn-block mybtn btn-primary tx-tfm">Acceder</button>
-                           </div>
-                           <div class="col-md-12 ">
-                              <div class="login-or">
-                                 <hr class="hr-or">
-                              </div>
-                           </div>
-                           <div class="form-group">
-                              <p class="text-center">¿No tienes cuenta? <a href="#" id="signup">Registrate aquí</a></p>
-                           </div>
                         </div>
-                        </form>
+                    </div>
 				</div>
             </div>
 		</div>
-      </div>
+    </div>
 
 </body>
