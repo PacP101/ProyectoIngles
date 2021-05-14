a0ff6e6e51230282d2f1c5cd033adcf3c01c3951
PacP101 <perezcamilo654@gmail.com>
2021-05-14 09:33:30 -0500

    s

--- resources/views/Inicio.blade.php
+++ resources/views/Inicio.blade.php
@@ -1,90 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <title>Basic English</title>
-</head>
-<body>
-
-
-  <!-- Nabvar -->
-<div style="background-color: #FA87BD">
- <div style="background-color:#51F8F3 ">
-    <div class="container">
-        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-
-            <a href="/" class="navbar-brand">
-            <img class="bi me-2" src="img/BEPNG.png" alt="" height="40" width="100" align = "left">
-          </a>
-            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">Contents</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">Contact</a></li>
-            </ul>
-
-          <div class="col-md-3 text-end">
-            <button type="button" class="btn btn-outline-primary me-2">Login</button>
-            <button type="button" class="btn btn-primary">Sign-up</button>
-            </div>
-          </div>
-        </header>
-      </div>
-       <!-- Nabvar -->
-
-        <!-- Imagen Horizontal -->
+@extends('layouts.navbar')
+@section('content')
+<!-- Imagen Horizontal -->
 
         <div class="px-4">
-        <div class="card mb-3" style="max-width: 1340px; background-color: #FAEE87">
+        <div class="card mb-3" style="max-width: 1340px;">
           <div class="row g-0">
             <div class="col-md-6">
               <img src="img/niños.jpg" alt="..." height="100%" width="100%">
             </div>
             <div class="col-md-6">
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <h3 class="card-title" style="font-family: Arial, Helvetica, Verdana, Geneva, Tahoma, sans-serif; text-align-last: center"> Welcome!</h3>
+                <p class="card-text" style="font-size: 20px ">Bievenidos a Basic English!
+                    Receive a welcome greeting to this virtual space, you are invited to explore and make the most of this interesting learning experience ...
+                    Change is always the end result of all true learning. Leo Buscaglia.
+                    Learning is not preparing for life. Learning is life itself. John Dewey.
+                     Set tough goals and don't stop until you get there (Bo Jackson)
+                     Success Dependence on Effort (Sophocles)
+                    Do your best. What you sow today will bear fruit tomorrow (Og Mandino)</p>
                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
               </div>
             </div>
           </div>
     </div>
   </div>
+<!-- Imagen Horizontal -->
 
-        <!-- Imagen Horizontal -->
-
-
-
- <!-- Metodologia -->
+<!-- Metodologia -->
  <div class="px-4">
     <div class="card mb-3" style="max-width: 1340px; ">
+     <h2 class="py-4" style="text-align-last: center">Structure and rules of the present simple</h2>
  <div class="row px-4 py-4">
     <div class="col-lg-4">
             <img src="img/E11.png" alt="" style="border-radius: 150px" width="150px">
             <h2>Affirmative</h2>
-            <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
-            <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
     </div><!-- /.col-lg-4 -->
     <div class="col-lg-4">
         <img src="img/E12.png" alt="" style="border-radius: 150px" width="150px">
         <h2>Negative</h2>
-        <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
-        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
     </div><!-- /.col-lg-4 -->
     <div class="col-lg-4">
         <img src="img/E13.png" alt="" style="border-radius: 150px" width="150px">
         <h2>Interrogative</h2>
-        <p>And lastly this, the third column of representative placeholder content.</p>
-        <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
     </div><!-- /.col-lg-4 -->
 </div><!-- /.row -->
 </div>
 
 </div>
 
-</body>
-</html>
+<footer class="blog-footer">
+    <p>Page created by <a href="https://www.unicesar.edu.co/index.php/es/noticias/aguachica?start=6">UPC Aguachica</a>
+    <p>
+      <a class="btn btn-outline-warning" href="#">Back to top</a>
+    </p>
+  </footer>
+
+@endsection
